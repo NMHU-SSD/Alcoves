5caa70f6a0c4d43ee7b386ceba74ca2e45189cd3
vallen97 <42785044+vallen97@users.noreply.github.com>
2020-06-03 14:42:08 -0600

    Made visual changes to press page
    Change press links into cards that contain more information about the articles.

--- pages/press.vue.js
+++ pages/press.vue.js
@@ -38,11 +38,31 @@ var Press = {
     },
   },
   template: `
+  <!--
   <div class="container-fluid mt-5 pt-5">
     <div class="row">
       <div class="col-sm-12 col-md-12 col-lg-12 mb-6" v-for="art in articles">
         <p class="d-inline"><b>{{ art.date }} - </b></p>
         <component class="d-inline" v-bind="linkProps(art.url)"> {{ art.title }} </component>
+      </div>
+    </div>
+  </div>
+  -->
+
+  <div class="container-fluid mt-5 pt-5">
+    <div class="row article">
+      <div class="col-sm-12 col-md-6 col-lg-4 mb-4" v-for="art in articles">
+        <div class="card" >
+          <div class="card-body">
+            <h5 class="card-title">{{ art.title }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted"><i>{{ art.publisher }}</i></h6>
+            <p class="card-text">
+              <b> {{ art.date }} </b> <br>
+              {{ art.author }}
+            </p>
+            <component class="article-link" v-bind="linkProps(art.url)"> Read Article </component>
+          </div>
+        </div>
       </div>
     </div>
   </div>
